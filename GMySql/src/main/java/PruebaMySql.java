0ec3ee91007855fd3d9797a507b828699adcab9f
JoseJota <jose.julian.perez.lucas@gmail.com>
2017-11-27 13:40:38 +0100

    Creado GMySql

--- GMySql/src/main/java/PruebaMySql.java
+++ GMySql/src/main/java/PruebaMySql.java
@@ -1,12 +1,32 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PruebaMySql {
 
-	public static void main(String[] args) throws SQLException {
-		// TODO Auto-generated method stub
-		Connection connection = DriverManager.getConnection("jdbc:mysql://loclhost/dbprueba","root","sistemas");
-	connection.close();
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		String DB_URL = "jdbc:mysql://localhost/dbprueba";
+		String USER = "root";
+		String PASS = "sistemas";
+		String sql;
+		   
+		Connection connection = DriverManager.getConnection(DB_URL,USER,PASS);
+		Statement stmt = connection.createStatement();
+
+	    sql = "SELECT * FROM categoria";
+	    ResultSet rs = stmt.executeQuery(sql);
+	    
+	    while(rs.next()){
+	         int id  = rs.getInt("id");
+	         String nombre = rs.getString("nombre");
+
+	         System.out.println("ID: " + id + " nombre: " + nombre);
+	      }
+	    
+	      rs.close();
+	      stmt.close();
+	      connection.close();
 	}
 }
